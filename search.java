ab6c817c5888b3f2d7c758cbaf5ad55aa3cd62ad
Elias Bergström <eliaselof@gmail.com>
2025-11-28 15:15:26 +0100

    MVP

--- search.java
+++ search.java
@@ -1,3 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class search {
-    
+    public static void main(String[] args) {
+        String inputText = args[0];
+        String inputFile = args[1];
+
+        try {
+            FileReader reader = new FileReader(inputFile);
+            BufferedReader br = new BufferedReader(reader);
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (line.contains(inputText)) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
