c6c807c92a3e749ed31c31dbaf93f622fe25be1a
Elias Bergström <eliaselof@gmail.com>
2025-12-04 13:28:20 +0100

    Fix

--- search.java
+++ search.java
@@ -3,6 +3,11 @@ import java.io.FileReader;
 
 public class search {
     public static void main(String[] args) {
+        
+        if(args.length > 2 || args[0] == null){
+            System.exit(1);
+        }
+        
         String inputText = args[0];
         String inputFile = args[1];
 
